62c88220eab248fa2b11b1a6c930da3df5e16c89
Artur Lages Lopes <arturllopes@gmail.com>
2026-05-19 12:00:10 -0300

    Feat: Cards das listas estruturadas

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { WelcomeSectionComponent } from "../welcome-section/welcome-section.component";
+import { TaskCardComponent } from "../task-card/task-card.component";
+import { TaskListSectionComponent } from "../task-list-section/task-list-section.component";
 
 @Component({
   selector: 'app-main-content',
-  imports: [WelcomeSectionComponent],
+  imports: [WelcomeSectionComponent, TaskCardComponent, TaskListSectionComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
