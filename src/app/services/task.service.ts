325aa9c08232a5f2c813b02e555837b9d035fb2b
Artur Lages Lopes <arturllopes@gmail.com>
2026-05-20 11:58:53 -0300

    Feat: Criação da logica de drag and drop do CDK

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,28 +1,30 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { ITask } from "../interfaces/task.interface";
-import { ITaskFormControls } from "../interfaces/task-form-controls.interface";
-import { TaskStatusEnum } from "../enums/task-status.enum";
-import { generateUniqueIdWithTimestamp } from "../utils/generate-unique-id-with-timestamps";
-
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { ITask } from '../interfaces/task.interface';
+import { ITaskFormControls } from '../interfaces/task-form-controls.interface';
+import { TaskStatusEnum } from '../enums/task-status.enum';
+import { generateUniqueIdWithTimestamp } from '../utils/generate-unique-id-with-timestamps';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class TaskService {
-
   private todoTask$ = new BehaviorSubject<ITask[]>([]);
-  readonly todoTask = this.todoTask$.asObservable();
+  readonly todoTask = this.todoTask$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   private doingTask$ = new BehaviorSubject<ITask[]>([]);
-  readonly doingTask = this.doingTask$.asObservable();
+  readonly doingTask = this.doingTask$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   private doneTask$ = new BehaviorSubject<ITask[]>([]);
-  readonly doneTask = this.doneTask$.asObservable();
+  readonly doneTask = this.doneTask$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   addTask(taskInfos: ITaskFormControls) {
-
     const newTask: ITask = {
       ...taskInfos,
       status: TaskStatusEnum.TODO,
@@ -33,5 +35,9 @@ export class TaskService {
     const currentList = this.todoTask$.value;
 
     this.todoTask$.next([...currentList, newTask]);
+  }
+
+  carregarListaAtualDeTodos(){
+    console.log('carregarListaAtualDeTodos', this.todoTask$.value)
   }
 }
